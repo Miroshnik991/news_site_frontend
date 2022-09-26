f85aaac8d38ab162955141d1d90213e6c4544606
Antony <miroshnikovanton2001@gmail.com>
2022-09-26 10:28:24 +0300

    first commit of auth

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -21,6 +21,7 @@ function MainPage() {
   const { posts, isFetching, error } = useSelector((state) => state.postsReducer);
   useEffect(() => {
     dispatch(createPostsLoader());
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
